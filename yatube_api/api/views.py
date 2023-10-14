f7a34c6b9cf2605be382e0d2281e6581e1f3e199
AlehAkbashev <akbashevaleh@gmail.com>
2023-10-14 20:09:11 +0300

    add limitoffsetpagination for Post

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,22 @@
 from rest_framework import viewsets, permissions
 from .serializers import PostSerializer, CommentSerializer, FollowSerializer, GroupSerializers
 from posts.models import Post, Comment, Follow, Group
+from django.shortcuts import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
 }
 
 SIMPLE_JWT = {
